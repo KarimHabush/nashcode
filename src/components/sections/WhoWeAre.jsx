a7a03aade7b9430e22024a21a9a382b1dca8721b
karimhabush <karim.habush@gmail.com>
2020-08-23 18:35:10 +0100

    Create WhoWeAre section without animations

---
+++ src/components/sections/WhoWeAre.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Typography, Grid } from '@material-ui/core';
+import KarimPic from './../../assets/DSCN3403.jpg'
+
+
+function WhoWeAre(props) {
+    const classes = useStyles();
+    return (
+        <div className={classes.container}>
+            <Container maxWidth="lg">
+                <Typography variant="h3" component="h2" className={classes.title}>
+                    WHO WE ARE?
+                </Typography>
+
+                <Typography variant="h5" component="p" className={classes.description}>
+                    We are a team of dreamers pushed by our curiosity
+                    <br />to experiment and create meaningful solutions using what we learned.
+                </Typography>
+
+
+
+                <Grid container >
+                    <Grid item xs={1} />
+
+                    <Grid item xs={4} >
+
+                        <img src={KarimPic} width="100%" alt="Karim's picture" />
+                        <Typography variant="h5" component="p" className={classes.presentations}>
+                            <span className={classes.bold}>NADA DABACH</span> is a Big Data student, lorem ipsum.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={2} />
+
+                    <Grid item xs={4} >
+
+                        <img src={KarimPic} width="100%" alt="Karim's picture" />
+                        <Typography variant="h5" component="p" className={classes.presentations}>
+                            <span className={classes.bold}>NADA DABACH</span> is a Big Data student, lorem ipsum.
+                            </Typography>
+                    </Grid>
+                    <Grid item xs={1} />
+
+                </Grid>
+                <Grid container >
+                    <Grid item xs={1} />
+
+                    <Grid item xs={4} >
+
+                        <img src={KarimPic} width="100%" alt="Karim's picture" />
+                        <Typography variant="h5" component="p" className={classes.presentations}>
+                            <span className={classes.bold}>NADA DABACH</span> is a Big Data student, lorem ipsum.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={2} />
+
+                    <Grid item xs={4} >
+
+                        <img src={KarimPic} width="100%" alt="Karim's picture" />
+                        <Typography variant="h5" component="p" className={classes.presentations}>
+                            <span className={classes.bold}>NADA DABACH</span> is a Big Data student, lorem ipsum.
+                            </Typography>
+                    </Grid>
+                    <Grid item xs={1} />
+
+                </Grid>
+            </Container>
+        </div>
+    )
+}
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        backgroundColor: '#0F0A3C',
+        textAlign: 'center',
+        padding: '100px 0 40px 0'
+    },
+    title: {
+        color: "#f9f9f9",
+        fontWeight: 600,
+        marginBottom: 60
+    },
+    description: {
+        color: '#f9f9f9',
+        marginBottom: 100
+    },
+    presentations: {
+        color: '#f9f9f9',
+        textAlign: 'left',
+        padding: 20,
+        paddingBottom: 60
+    },
+    bold: {
+        fontWeight: 600
+    }
+}));
+
+export default WhoWeAre;
