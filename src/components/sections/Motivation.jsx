1d5ae3513061dc7d5d4f9b940dfab6e738d0b1ed
karimhabush <karim.habush@gmail.com>
2020-08-23 18:36:43 +0100

    Create Motivation section without animations

---
+++ src/components/sections/Motivation.jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Typography, Grid } from '@material-ui/core';
+import StarIcon from './../../assets/value_star.svg';
+import GiftIcon from './../../assets/value_gift.svg';
+import DiamondIcon from './../../assets/value_diamond.svg';
+import LineCircle from './../../assets/line_circle_dark.svg';
+const valuesList = [
+    [{
+        id: 0,
+        icon: StarIcon,
+        title: 'Data Science',
+
+    },
+    {
+        id: 1,
+        icon: GiftIcon,
+        title: 'Front End',
+    },
+    {
+        id: 2,
+        icon: DiamondIcon,
+        title: 'Back End',
+    },
+    {
+        id: 3,
+        icon: StarIcon,
+        title: 'Project Management',
+    }],
+    [{
+        id: 4,
+        icon: StarIcon,
+        title: 'Data Science',
+
+    },
+    {
+        id: 5,
+        icon: StarIcon,
+        title: 'Front End',
+    },
+    {
+        id: 6,
+        icon: StarIcon,
+        title: 'Back End',
+    },
+    {
+        id: 7,
+        icon: StarIcon,
+        title: 'Project Management',
+    }]
+]
+
+
+
+function WhoWeAre(props) {
+    const classes = useStyles();
+    return (
+        <div className={classes.container}>
+            <div className={classes.lineCircle} >
+
+                <img width='450' src={LineCircle} />
+            </div>
+            <Container maxWidth="lg">
+                <Typography variant="h3" component="h2" className={classes.title}>
+                    WHAT MOTIVATES US?
+                </Typography>
+                <Typography variant="h5" component="p" className={classes.description}>
+                    We are a team of dreamers pushed by our curiosity
+                    <br />to experiment and create meaningful solutions using what we learned.
+                </Typography>
+
+
+
+                {
+                    valuesList.map(values => (
+                        <Grid container spacing={5} className={classes.valueContainer}>
+
+                            {values.map(value => (
+                                <Grid item xs={3} key={value.id}>
+
+                                    <img height="40" src={value.icon} />
+                                    <Typography variant="h5" component="p" className={classes.valueTitle}>
+                                        {value.title}
+                                    </Typography>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    ))
+                }
+
+            </Container>
+        </div>
+    )
+}
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        backgroundColor: '#f9f9f9',
+        textAlign: 'center',
+        padding: '100px 0 100px 0',
+        position: 'relative'
+    },
+    title: {
+        color: "#0F0A3C",
+        fontWeight: 600,
+        marginBottom: 60
+    },
+    description: {
+        color: '#404040',
+        marginBottom: 100
+    },
+    presentations: {
+        color: '#f9f9f9',
+        textAlign: 'left',
+        padding: 20,
+        paddingBottom: 60
+    },
+    bold: {
+        fontWeight: 600
+    },
+    valueTitle: {
+        fontWeight: 600,
+        marginTop: 20,
+        color: '#0F0A3C'
+    },
+    valueContainer: {
+        padding: '30px 0',
+    },
+    lineCircle: {
+        color: "#000000",
+        position: 'absolute',
+        left: 0,
+        top: -70
+    }
+}));
+
+export default WhoWeAre;
